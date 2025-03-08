a51d48cf719e06af8dd58aa522db1b95127229da
Asindu De Silva <asindudesilva@gmail.com>
2025-03-08 10:16:12 +0530

    backend init

---
+++ spirit11 - backend/src/main/java/com/example/spirit11/Spirit11BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirit11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spirit11BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spirit11BackendApplication.class, args);
+	}
+
+}
---
+++ spirit11 - backend/src/test/java/com/example/spirit11/Spirit11BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirit11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spirit11BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
